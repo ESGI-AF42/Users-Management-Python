29b8625fad8d9a5dc3365564952695b2aa09216b
DylanQuiss <90315536+DylanQuiss@users.noreply.github.com>
2021-09-19 12:14:10 +0200

    Create Direction.py

---
+++ code/Direction.py
@@ -0,0 +1,28 @@
+from corp import Corp
+class Direction():
+
+    def_init_(self, Age, Phone, Email, Login, Password, Salary):
+        self.age = Age
+        self.phone = Phone
+        self.email = Email
+        self.login = Login
+        self.mpassword = Password
+        self.salary = Salary
+
+    def get_age(self):
+        return self.age
+    
+    def get_phone(self):
+        return self.phone
+    
+    def get_email(self):
+        return self.email
+
+    def get_login(self):
+        return self.login
+
+    def get_password(self):
+        return self.password
+
+    def get_salary(self):
+        return self.salary
