9e4d24fbfa435453fb2f0feefb1ff6c307d82d0c
DylanQuiss <90315536+DylanQuiss@users.noreply.github.com>
2021-09-19 12:28:07 +0200

    Update Members.py

--- code/Members.py
+++ code/Members.py
@@ -11,26 +11,26 @@ class Member(Corp):
         self.login = Login
         self.password = Password
 
-    def get_entreprise_name(self):
+    def get_member_name(self):
         return self.name
 
-    def get_entreprise_firstname(self):
+    def get_member_firstname(self):
         return self.firstname
     
-    def get_entreprise_age(self):
+    def get_member_age(self):
         return self.age
 
-    def get_entreprise_post(self):
+    def get_member_post(self):
         return self.post
 
-    def get_entreprise_phone(self):
+    def get_member_phone(self):
         return self.phone
 
-    def get_entreprise_email(self):
+    def get_member_email(self):
         return self.email
     
-    def get_entreprise_login(self):
+    def get_member_login(self):
         return self.login
 
-    def get_entreprise_password(self):
+    def get_member_password(self):
         return self.password
